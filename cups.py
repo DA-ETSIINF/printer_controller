6a6d778230cd26d1c841cf9833ecb45f4be5308b
max <maximo.garcia.martinez@alumnos.upm.es>
2018-12-14 11:40:49 +0100

    added lp options

--- cups.py
+++ cups.py
@@ -24,79 +24,96 @@ import subprocess
 
 import urllib.request
 
-printer_name = ['Lexmark_Lexmark_International_Lexmark_MS510dn']
+printers = {
+    'default': 'Lexmark_Lexmark_International_Lexmark_MS510dn'
+}
 username = 'username'
 password = 'password'
 
+
 class S(BaseHTTPRequestHandler):
-	def _set_response(self):
-		self.send_response(200)
-		self.send_header('Content-type', 'application/json')
-		self.end_headers()
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
 
-	def do_GET(self):
-		logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-		self._set_response()
-		self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n",
+                     str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(
+            self.path).encode('utf-8'))
 
-	def do_POST(self):
-		content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-		post_data = self.rfile.read(content_length) # <--- Gets the data itself
+    def do_POST(self):
+        # <--- Gets the size of data
+        content_length = int(self.headers['Content-Length'])
+        # <--- Gets the data itself
+        post_data = self.rfile.read(content_length)
 
-		try:
-			jsondata = json.loads(post_data)[0]
+        try:
+            jsondata = json.loads(post_data)[0]
 
-			if jsondata["username"] == username and jsondata['password'] == password:
-				# set file name, retrieved from post request
-				filename = jsondata['filename']
-				# download file to print
-				urllib.request.urlretrieve(jsondata["url"], 'media/'+filename)
+            if jsondata["username"] == username and jsondata['password'] == password:
+                # set file name, retrieved from post request
+                filename = jsondata['filename']
 
+                # download file to print
+                urllib.request.urlretrieve(jsondata["url"], 'media/'+filename)
 
-				#send file to printer
-				subprocess.run("lp media/" + filename + " -d " + printer_name[0], shell=True)
+                # configure lp options
+                lp_config = {
+                    "filename": jsondata["filename"],
+                    "printer_name": printers["default"],
+                    "ncopies": jsondata["ncopies"] if jsondata != None else 1
+                }
 
+                # prepare command
+                lp_command = "lp media/{filename} -d {printer_name} -n {ncopies}".format_map(
+                    **lp_config)
 
-				# Delete printed file
-				subprocess.run("rm media/" + filename, shell=True)
+                # send file to printer
+                subprocess.run(lp_command, shell=True)
 
-				logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-						 	str(self.path), str(self.headers), post_data)
-				self._set_response()
-				self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
-		except:
-			print("Exception ocurred")
-			logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-						 str(self.path), str(self.headers), post_data)
+                # Delete printed file
+                subprocess.run("rm media/" + filename, shell=True)
 
-			self._set_response()
-			self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
-			return
+                logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                             str(self.path), str(self.headers), post_data)
+                self._set_response()
+                self.wfile.write("POST request for {}".format(
+                    self.path).encode('utf-8'))
+        except:
+            print("Exception ocurred")
+            logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                         str(self.path), str(self.headers), post_data)
 
+            self._set_response()
+            self.wfile.write("POST request for {}".format(
+                self.path).encode('utf-8'))
+            return
 
 
 def run(server_class=HTTPServer, handler_class=S, port=8081):
-	logging.basicConfig(level=logging.INFO)
-	server_address = ('', port)
-	httpd = server_class(server_address, handler_class)
-	logging.info('Starting httpd...\n')
-	try:
-		httpd.serve_forever()
-	except KeyboardInterrupt:
-		pass
-	httpd.server_close()
-	logging.info('Stopping httpd...\n')
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
 
 
 # Before starting the server, check if the default port has been changed and if media folder exists
 if __name__ == '__main__':
-	from sys import argv
+    from sys import argv
 
-	if len(argv) == 2:
-		run(port=int(argv[1]))
-	else:
-		if not os.path.exists("media"):
-			os.makedirs('media')
-		else:
-			run()
-
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        if not os.path.exists("media"):
+            os.makedirs('media')
+        else:
+            run()
